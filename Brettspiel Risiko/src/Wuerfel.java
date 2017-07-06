99d5e18659b1bb7bde267b1b08556471526779d9
Konrad-Gamer <Konrad-Gamer@Konrad-Gamer-PC.fritz.box>
2017-07-06 21:50:27 +0200

    Risiko.jpg in Image ordner

--- Brettspiel Risiko/src/Wuerfel.java
+++ Brettspiel Risiko/src/Wuerfel.java
@@ -5,9 +5,15 @@ public class Wuerfel extends Controll {
 
 
 	/**
-	 * Diese Klasse ist fÃ¼r das wÃ¼rfeln zustÃ¤ndig;
+	 * Diese Klasse ist für das würfeln zuständig;
+	 * 
+	 * Die Methode "Rolls" ruft die Methode "Roll" auf
+	 * Der Angreifer würfelt höchstens 3 mal (Risiko Regeln) Wenn der Angreifer aber mehr Truppen hat wird durch die Schleife die zuständige Variable auf 3 gesetzt um 3 mal zu würfeln
 	 * 
 	 * 
+	 * ES Fehlt NOCH : -Überlegung ob man 2 Methoden macht : eine für denn Angreifer und eine für den Verteidiger ??? 
+	 * 				   -die Entsprechen Variable mit der Truppen anzahl vom Spielablauf
+	 * 				   - Testen
 	 */
 	
 	public Wuerfel(){
@@ -33,7 +39,7 @@ public class Wuerfel extends Controll {
 			 Anz_Wuerfel =  Truppen; 
 		}
 		
-		
+		// For Schliefe für das Würfeln
 		
 		
 		for(Anz_Wuerfel++ ; Anz_Wuerfel <= 0; Anz_Wuerfel--)
