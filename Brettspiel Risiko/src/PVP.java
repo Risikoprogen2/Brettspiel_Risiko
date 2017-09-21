fd2b7308fd25389be399761ededb0e21ee2d6ba8
Robin <robin@Robin-PC.fritz.box>
2017-09-21 17:30:34 +0200

    test

--- Brettspiel Risiko/src/PVP.java
+++ Brettspiel Risiko/src/PVP.java
@@ -1,14 +1,15 @@
 import java.awt.Color;
 
 public class PVP {
-
+	boolean a=true;
 	private SpielKarte1 SpielKarte1;
 	  public void Button() {
 		  SpielKarte1 = new SpielKarte1();
 	  }
 	  public void doSomething() {
-	    SpielKarte1.button_5.setBackground(Color.red);
-	    
+	    if (a=true){
+	    	SpielKarte1.button_5.setBackground(Color.red);
+	    }
 	    //WARUM????
 	  }
 	}
