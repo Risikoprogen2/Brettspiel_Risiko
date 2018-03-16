e9b33bec3788cf57f861a4fcad9b393320bc7df8
Konrad <konrad.musiol@stud.hs-owl.de>
2018-03-16 01:30:53 +0100

    0.003
    So wird abgegeben

--- Brettspiel Risiko/src/Wuerfel.java
+++ Brettspiel Risiko/src/Wuerfel.java
@@ -5,30 +5,20 @@ public class Wuerfel {
 
 
 	/**
-	 * Diese Klasse ist f�r das w�rfeln zust�ndig;
-	 * 
+	 * Diese Klasse ist fuer das wuerfeln zustaendig;
+	 * @Autor Konrad
 	 * Die Methode "Rolls" ruft die Methode "Roll" auf
-	 * Der Angreifer w�rfelt h�chstens 3 mal (Risiko Regeln) Wenn der Angreifer aber mehr Truppen hat wird durch die Schleife die zust�ndige Variable auf 3 gesetzt um 3 mal zu w�rfeln
-	 * 
-	 * 
-	 * ES Fehlt NOCH : 
-	 * 				   -die Entsprechen Variable mit der Truppen anzahl vom Spielablauf
-	 * 				   - Testen
+	 * Der Angreifer greift hoechstens mit 3 Truppen an, Wenn der Angreifer aber mehr Truppen hat wird durch die Schleife die zustaendige Variable auf 3 gesetzt um mit 3 wuerfel zu wuerfeln 
 	 */
 	
 	public Wuerfel(){
 		
-		int Wuerfel;
-		int Truppen;
-		int Anz_Wuerfel;
-
 	}
-	// Verteidiger w�rfelt 
-	public int RollsDefend(int Truppen){
-		
-		
-		int Anz_Wuerfel = 0;
-		int ergD = 0;
+	// Verteidiger wuerfelt 
+	public int RollsDefend(int Truppen){// Aus Controll wird die anzahl der Truppen übergeben
+	
+		int Anz_Wuerfel = 0; // Anzahl der Wuerfel
+		int ergD = 0;	// Ergebnis welches die Klasse ausgibt
 		
 		if (Truppen > 2) {
 			
@@ -38,7 +28,7 @@ public class Wuerfel {
 			 Anz_Wuerfel =  Truppen; 
 		}
 		
-		// For Schliefe f�r das W�rfeln
+		// For Schliefe fuer das wuerfeln
 		
 		
 		for(Anz_Wuerfel++ ; Anz_Wuerfel <= 0; Anz_Wuerfel--)
@@ -51,12 +41,10 @@ public class Wuerfel {
 		return ergD;
 	}
 	
-	// Angreifer W�rfelt
+	// Angreifer wuerfelt
 	public int RollsFight(int Truppen){
 		
 		// diese Klasse soll die Methode Roll() so oft aufrufen wie es Truppen gibt
-		
-		// Die Variable int i muss noch abgeändert werden damit das richtig funktioniert.
 		
 		int Anz_Wuerfel = 0;
 		int ergF = 0;
@@ -77,19 +65,12 @@ public class Wuerfel {
 			ergF = ergF + Roll();
 			
 		}
-		
-		//System.out.println(Roll());
-		return ergF;
+		return ergF; // wuerfel ergenis
 	}
-	
-	
+		
 	// Diese Methode erzeugt eine einfache zufalls zahl zwischen 1 und 6 und syblolisiert damit einen Würfel.
 public int Roll(){	
 	int Wuerfel = (int)((Math.random()) * 6 + 1);
 	return Wuerfel;
 }
-	
-	
-	
-	
 }
